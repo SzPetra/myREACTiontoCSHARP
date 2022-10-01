c1067c03a03d7bbb4e68f798db52e4b536e4fc56
KisLorand <kislorand270@gmail.com>
2022-10-01 17:31:32 +0200

    testing the fetch

---
+++ CodeToGiveTests/CodeToGiveTests/ClientApp/src/components/TestData.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import ToggleBtn from './ToggleBtn';
+import apiRequest from '../requests/apiRequest';
+import apiRequestOptions from '../requests/apiRequest';
+import { useState, useEffect } from 'react';
+
+const TestData = () => {
+const [urle, seturle] = useState("tutya");
+  
+
+    if (urle === "tutya") {
+        return (
+            <ToggleBtn toggle={[urle, seturle]}/>
+        );
+    } else {
+        return (
+        <div>
+            TestData
+            <ToggleBtn toggle={[urle, seturle]}/>
+            {urle}
+        </div>
+        )
+    }
+};
+
+export default TestData;
---
+++ CodeToGiveTests/CodeToGiveTests/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeToGiveTests.Encryption;
+
+namespace CodeToGiveTests.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = StringCrypter.Encrypt("tutya")//StringCipher.Encrypt("username=John", "tutya27")//Summaries[Random.Shared.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+
+		[HttpPost("encrypt")]
+		public string GetAlles([FromBody] string payload)
+		{
+			return StringCrypter.Encrypt(payload);
+		}
+	}
+}
