531d89da23431ad1ed495785d0ba49ff45ed6a6f
SzPetra <petra.szilagyi27@gmail.com>
2022-10-01 20:39:02 +0200

    fix react routing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";
 import CreateLink from "./createLinkPage/components/CreateLink";
+import AdminPage from "./adminPage/AdminPage";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +12,8 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/tests" element={<App />}>
-          <Route exact path="/create-link" element={<CreateLink />} />
+          <Route index element={<AdminPage />} />
+          <Route exact path="create-link" element={<CreateLink />} />
         </Route>
       </Routes>
     </BrowserRouter>
