874d200d1d869e66c0753d22449dcd9dc6259328
Imreszab <szabimi12@gmail.com>
2022-10-01 20:20:25 +0200

    add email sending feature

---
+++ CodeToGiveTests/CodeToGiveTests/Models/EmailAttachment.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeToGiveTests.Models
+{
+    public class EmailAttachment
+    {
+        public string Name { get; set; } = String.Empty;
+        public string ContentType { get; set; } = String.Empty;
+
+        public byte[] Data { get; set; } = new byte[0];
+    }
+}
---
+++ CodeToGiveTests/CodeToGiveTests/services/MailJetProvider.cs
@@ -0,0 +1,61 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using System;
+using Newtonsoft.Json.Linq;
+using CodeToGiveTests.Interfaces;
+using CodeToGiveTests.Models;
+
+namespace CodeToGiveTests.services
+{
+    public class MailJetProvider : EmailSenderService, IEmailSender
+    {
+        protected override async Task Send(EmailModel email)
+        {
+            try
+            {
+                JArray jArray = new JArray();
+                JArray attachments = new JArray();
+                if(email.Attachments != null && email.Attachments.Count() > 0)
+                {
+                    email.Attachments.ToList().ForEach(attachment => attachments.Add(
+                        new JObject
+                        {
+                            new JProperty("Content-type", attachment.ContentType),
+                            new JProperty("Filename", attachment.Name),
+                            new JProperty("Content", Convert.ToBase64String(attachment.Data))
+                        }
+                        ));
+                }
+                jArray.Add(new JObject
+                {
+                    new JProperty("FromEmail", "szabimi12@gmail.com"), //Email registered with mailJet
+                    new JProperty("FromName", "Imre Szabó"),
+                    new JProperty("Recipients", new JArray
+                    {
+                        new JObject
+                        {
+                            new JProperty("Email", email.EmailAdress),
+                            new JProperty("Name", email.EmailAdress)
+                        }
+                    }),
+                    new JProperty("Subject", email.Subject),
+                    new JProperty("Text-part", email.Body),
+                    new JProperty("Html-part", email.Body),
+                    new JProperty("Attachments", attachments)
+
+                });
+                var client = EmailSenderService.CreateMailJetClient();
+                var request = new MailjetRequest
+                {
+                    Resource = Mailjet.Client.Resources.Send.Resource
+                }
+                .Property(Mailjet.Client.Resources.Send.Messages, jArray);
+                var response = await client.PostAsync(request);
+                Console.WriteLine($"Send result {response.StatusCode} with message {response.Content}");
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+    }
+}
