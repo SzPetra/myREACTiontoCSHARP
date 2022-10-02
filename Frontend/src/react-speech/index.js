4a1e1a70cd16e3393497350d0cd1377ed183c64a
SzPetra <petra.szilagyi27@gmail.com>
2022-10-02 19:00:00 +0200

    add react speech files to project

---
+++ Frontend/src/react-speech/index.js
@@ -0,0 +1,4 @@
+// Original: https://github.com/MikeyParton/react-speech-kit
+
+export { default as useSpeechRecognition } from "./useSpeechRecognition";
+export { default as useSpeechSynthesis } from "./useSpeechSynthesis";
