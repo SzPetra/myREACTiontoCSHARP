164e6c4ade7cf0bfa196c177c5f448abcd2b77b1
Imreszab <szabimi12@gmail.com>
2022-10-02 17:42:57 +0200

    add restructuring project

---
+++ Backend/CodeToGveTests/Program.cs
@@ -0,0 +1,51 @@
+using CodeToGiveTests.services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<EmailHostedService>();
+builder.Services.AddHostedService(provider => provider.GetService<EmailHostedService>());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("Policy1",
+		policy =>
+		{
+			policy.WithOrigins("*",
+								"*")
+				.AllowAnyHeader()
+				.AllowAnyMethod();
+		});
+
+	options.AddPolicy("AnotherPolicy",
+		policy =>
+		{
+			policy.WithOrigins("*")
+								.AllowAnyHeader()
+								.AllowAnyMethod();
+		});
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Frontend/src/createLinkPage/components/CreateLink.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Navbar from "../../layout/components/Navbar";
+import "../assets/createLinkPage.css";
+import Select from "./Select";
+import testPageOptions from "../../options/testPageOptions";
+import modeOptions from "../../options/modeOptions";
+import Form from "./Form";
+const CreateLink = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="page-content-container">
+        <img
+          src="/we-help-people.png"
+          id="we-help-people-image"
+          alt="We help people with disabilities"
+        />
+
+        <form className="form">
+          <section className="form-content">
+            {/* email input */}
+            <label for="email">Enter a valid email address*:</label>
+            <input
+              id="email"
+              type="email"
+              required
+              placeholder="email of the receiver"
+            />
+
+            {/* select test type */}
+            <label for="test-type">Choose a type of test* :</label>
+            <Select id="test-type" options={testPageOptions} />
+
+            {/* select mode */}
+            <label for="mode">
+              Choose mode:
+              <p>(if you don't choose test mode, the default will be normal)</p>
+            </label>
+            <Select id="mode" options={modeOptions} />
+          </section>
+          <Form />
+          <button type="submit" id="form-btn">
+            Send test link
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default CreateLink;
