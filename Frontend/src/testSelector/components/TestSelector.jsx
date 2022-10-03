d2f650c36f52e9c0c85ea157c29fa4d0c87d17cb
KisLorand <kislorand270@gmail.com>
2022-10-03 14:33:58 +0200

    add selectTestType method

--- Frontend/src/testSelector/components/TestSelector.jsx
+++ Frontend/src/testSelector/components/TestSelector.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import testPageOptions from '../../options/testPageOptions';
+import ChairLampTestPage from '../../testPage/chairLampTest/components/ChairLampTestPage';
+import WorkMotivationTestPage from '../../testPage/workMotivationTest/components/WorkMotivationTestPage';
 
 const TestSelector = () => {
     const [test, setTest] = useState(null);
 
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
-    console.log(params.data + "dddd")
+    console.log(params.data + "|")
     const joinedString = params.data.replace(/ /g,'+');
     useEffect(
       ()=>{
@@ -15,15 +18,21 @@ const TestSelector = () => {
     .then((data) => setTest(data));
       },
       []);
-    
-  return (test ?
-    <div>
-      <p>{test.name}</p>
-      <p>{test.testType}</p>
-      <p>{test.viewMode}</p>
-      </div> :
-    <p>Loading...</p>
-  )
+    return (test ?
+      selectTestType(test) :
+      <p>Loading...</p>
+      );
+};
+
+function selectTestType(test) {
+  for (let i = 0; i < testPageOptions.length; i++)
+  {
+    if (test.testType === testPageOptions[i].option)
+    {
+      return testPageOptions[i].component;
+    }
+  };
+  return null;
 };
 
 export default TestSelector;
