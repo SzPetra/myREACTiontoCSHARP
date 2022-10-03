24d7d606f23ec8bf68c7695afbc0ccfebf23bb95
Imreszab <szabimi12@gmail.com>
2022-10-03 16:20:32 +0200

    fix import in index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import CreateLink from "./createLinkPage/components/CreateLink";
 import AdminPage from "./adminPage/components/AdminPage";
 import App from "./App";
-import WorkMotivationTestPage from "./testPage/workMotivationTest/components/WorkMotivationTestPage";
+import WorkMotivationTestPage from "./testPage/workMotivationTest/components/hungarian/WorkMotivationTestPage";
 import ChairLampTestPage from "./testPage/chairLampTest/components/ChairLampTestPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
