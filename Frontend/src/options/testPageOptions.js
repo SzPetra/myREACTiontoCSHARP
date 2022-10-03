01b6157ce81dfcf058cb1bae4ab493102f84c1f5
KisLorand <kislorand270@gmail.com>
2022-10-03 14:28:32 +0200

    add component prop

--- Frontend/src/options/testPageOptions.js
+++ Frontend/src/options/testPageOptions.js
@@ -1,13 +1,18 @@
+import ChairLampTestPage from "../testPage/chairLampTest/components/ChairLampTestPage";
+import WorkMotivationTestPage from "../testPage/workMotivationTest/components/WorkMotivationTestPage";
+
 const testPageOptions = [
   {
     id: 1,
     option: "Chair-lamp test",
     pageLink: "TODO",
+    component: <ChairLampTestPage />,
   },
   {
     id: 2,
     option: "Work motivation test",
     pageLink: "TODO",
+    component: <WorkMotivationTestPage />,
   },
 ];
 
