d83e21aa4b7d60bed282db223a2da39a28b4ac3c
KisLorand <kislorand270@gmail.com>
2022-10-04 15:18:03 +0200

    add Context.Providers

--- Frontend/src/testSelector/components/TestSelectorPage.jsx
+++ Frontend/src/testSelector/components/TestSelectorPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import TestSelector from './TestSelector';
 import { useState } from 'react';
+import { TestDataContext, TestFinishedContext } from './TestFinishedContext';
+import { Outlet } from 'react-router-dom';
+import TestSelector from './TestSelector';
+import TestEndPage from './TestEndPage';
 
 function TestSelectorPage() {
-   {/* const [testComplete, setTestComplete]= useState(false);
-   return ( testComplete ?
-    <><TestEndPage /></> :
-    <><TestSelector /></>
-   );
+    const [testComplete, setTestComplete] = useState(false);
+    const [testResults, setTestResults] = useState(null);
+   
+   {/*
   */}
   return (
-    <>
-      <TestSelector />
-    </>
+      <TestFinishedContext.Provider value={{testComplete, setTestComplete}}>
+        <TestDataContext.Provider value={{testResults, setTestResults}}>
+          {testComplete ?
+            <TestEndPage /> :
+            <TestSelector />
+          }
+          <Outlet /> {/*  */}
+        </TestDataContext.Provider>
+      </TestFinishedContext.Provider>
   )
 };
 
