09d01f1a3f6ad1133768b44fb9889f3708563546
KisLorand <kislorand270@gmail.com>
2022-10-04 17:52:14 +0200

    change parameter object

--- Backend/CodeToGveTests/PDFGeneration/PdfGenerator.cs
+++ Backend/CodeToGveTests/PDFGeneration/PdfGenerator.cs
@@ -7,12 +7,13 @@ using PdfSharp.Pdf.IO;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using CodeToGiveTests.Models;
+using CodeToGveTests.Models;
 
 namespace CodeToGveTests.PDFGeneration
 {
 	public class PdfGenerator
 	{
-		public static PdfDocument GeneratePdf(LoadModel payload)
+		public static PdfDocument GeneratePdf(TestResultModel payload)
 		{
 			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 			
@@ -28,12 +29,15 @@ namespace CodeToGveTests.PDFGeneration
 			new XRect(0, 0, page.Width, page.Height),
 			XStringFormats.Center);
 			
-			string name = payload.Name; //catch name is null
+			string name = payload.Name; //catch if name is null
 			string testType = "Chiar-lamp test";
 
 			string filename = $"{name}_{testType}.pdf";
 
 			document.Save($"../CodeToGveTests/PdfStorage/{filename}");
+
+			Process.Start(filename);
+
 			return document;
 		}
 	}
