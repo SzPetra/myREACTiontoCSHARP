addb6ee865e462c7d5ea32ed6516138a615c2ea7
KisLorand <kislorand270@gmail.com>
2022-10-04 15:17:03 +0200

    add Context import

--- Frontend/src/testSelector/components/TestSelector.jsx
+++ Frontend/src/testSelector/components/TestSelector.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useContext } from 'react';
+
 import testPageOptions from '../../options/testPageOptions';
+import { TestFinishedContext } from './TestFinishedContext';
 
 const TestSelector = () => {
     const [test, setTest] = useState(null);
@@ -9,13 +12,15 @@ const TestSelector = () => {
     const params = Object.fromEntries(urlSearchParams.entries());
     console.log(params.data + "|")
     const joinedString = params.data.replace(/ /g,'+');
+
     useEffect(
       ()=>{
         fetch(`https://localhost:7200/api/Email/DecryptUrl?payload=${joinedString}`)
-    .then((response) => response.json())
-    .then((data) => setTest(data));
+        .then((response) => response.json())
+        .then((data) => setTest(data));
       },
       []);
+
     return (test ?
       selectTestType(test) :
       <p>Loading...</p>
