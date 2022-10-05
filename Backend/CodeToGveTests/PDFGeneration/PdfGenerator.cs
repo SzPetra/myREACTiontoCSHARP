5e9ade924e42afe45c982d7163e64139f190cd3f
KisLorand <kislorand270@gmail.com>
2022-10-05 16:38:39 +0200

    add test methods

--- Backend/CodeToGveTests/PDFGeneration/PdfGenerator.cs
+++ Backend/CodeToGveTests/PDFGeneration/PdfGenerator.cs
@@ -13,6 +13,8 @@ namespace CodeToGveTests.PDFGeneration
 {
 	public class PdfGenerator
 	{
+		private const int borderWidth = 2;
+
 		public static void GeneratePdf(TestResultModel payload)
 		{
 			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
@@ -25,14 +27,40 @@ namespace CodeToGveTests.PDFGeneration
 			PdfPage page = document.AddPage();
 			XGraphics gfx = XGraphics.FromPdfPage(page);
 			XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
-
-			gfx.DrawString(
-				$"Cilent Name : {name}!\n{payload.TestData}", 
-				font, 
+			int xBase = 22;
+			int x = 100;
+			int y = 30;
+			XRect rect = new XRect(22, 0, x,y);
+			XRect rect2 = new XRect(22, 22, x, y);
+			XRect rect3 = new XRect(22, xBase*2, x, y);
+			/*gfx.DrawString(
+				$"Cilent Name : {name}!",
+				font,
 				XBrushes.Black,
 				new XRect(0, 0, page.Width, page.Height),
-				XStringFormats.Center);
+				XStringFormats.Center);*/
+			WritePdfLine(
+				$"Test Type : ",
+				gfx,
+				font,
+				page,
+				rect);
+
+			WritePdfLine(
+				$"Cilent Name : {name}",
+				gfx,
+				font,
+				page,
+				rect2);
 			
+			WritePdfLine(
+				payload.TestData,
+				gfx,
+				font,
+				page,
+				rect3);
+
+
 			string testType = "Chair-lamp test";
 
 			string filename = $"{name}_{testType}.pdf";
@@ -59,5 +87,69 @@ namespace CodeToGveTests.PDFGeneration
 			document.Save($"C/Users/Kis Lóránd/projects_advanced/codeToGive/Backend/CodeToGveTests/PdfStorage/{filename}");
 			//document.Save($"../CodeToGveTests/PdfStorage/{filename}");*/
 		}
+
+		private static void WritePdfLine(string text, XGraphics gfx, XFont font, PdfPage page, XRect rect)
+		{
+			gfx.DrawString(
+				text,
+				font,
+				XBrushes.Black,
+				rect,
+				XStringFormats.Center);
+		}
+
+		/*void DrawText(XGraphics gfx, int number)
+		{
+			BeginBox(gfx, number, "Text Styles");
+
+			const string facename = "Times New Roman";
+
+			//XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always);
+			XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.WinAnsi, PdfFontEmbedding.Default);
+
+			XFont fontRegular = new XFont(facename, 20, XFontStyle.Regular, options);
+			XFont fontBold = new XFont(facename, 20, XFontStyle.Bold, options);
+			XFont fontItalic = new XFont(facename, 20, XFontStyle.Italic, options);
+			XFont fontBoldItalic = new XFont(facename, 20, XFontStyle.BoldItalic, options);
+
+			// The default alignment is baseline left (that differs from GDI+)
+			gfx.DrawString("Times (regular)", fontRegular, XBrushes.DarkSlateGray, 0, 30);
+			gfx.DrawString("Times (bold)", fontBold, XBrushes.DarkSlateGray, 0, 65);
+			gfx.DrawString("Times (italic)", fontItalic, XBrushes.DarkSlateGray, 0, 100);
+			gfx.DrawString("Times (bold italic)", fontBoldItalic, XBrushes.DarkSlateGray, 0, 135);
+
+			EndBox(gfx);
+		}
+
+		public void BeginBox(XGraphics gfx, int number, string title)
+		{
+			const int dEllipse = 15;
+			XRect rect = new XRect(0, 20, 300, 200);
+			if (number % 2 == 0)
+				rect.X = 300 - 5;
+			rect.Y = 40 + ((number - 1) / 2) * (200 - 5);
+			rect.Inflate(-10, -10);
+			XRect rect2 = rect;
+			rect2.Offset(borderWidth, borderWidth);
+			gfx.DrawRoundedRectangle(new XSolidBrush(this.shadowColor), rect2, new XSize(dEllipse + 8, dEllipse + 8));
+			XLinearGradientBrush brush = new XLinearGradientBrush(rect, this.backColor, this.backColor2, XLinearGradientMode.Vertical);
+			gfx.DrawRoundedRectangle(this.borderPen, brush, rect, new XSize(dEllipse, dEllipse));
+			rect.Inflate(-5, -5);
+
+			XFont font = new XFont("Verdana", 12, XFontStyle.Regular);
+			gfx.DrawString(title, font, XBrushes.Navy, rect, XStringFormats.TopCenter);
+
+			rect.Inflate(-10, -5);
+			rect.Y += 20;
+			rect.Height -= 20;
+
+			this.state = gfx.Save();
+			gfx.TranslateTransform(rect.X, rect.Y);
+		}
+
+		public void EndBox(XGraphics gfx)
+		{
+			gfx.Restore(this.state);
+		}*/
 	}
 }
